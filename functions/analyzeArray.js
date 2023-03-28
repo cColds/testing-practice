6aeafddf0a4008ba327318a75c0ded4b61e26a34
cColds <103373668+cColds@users.noreply.github.com>
2023-03-28 05:35:38 -0500

    Pass analyze array tests

--- functions/analyzeArray.js
+++ functions/analyzeArray.js
@@ -1,1 +1,13 @@
-export default function analyzeArray(array) {}
+export default function analyzeArray(array) {
+	return {
+		average: average(array),
+		min: min(array),
+		max: max(array),
+		length: length(array),
+	};
+}
+
+const average = (array) => array.reduce((a, b) => a + b) / array.length;
+const min = (array) => Math.min(...array);
+const max = (array) => Math.max(...array);
+const length = (array) => array.length;
